fd5e2618754134db452f4af2dfdd7edc5603904f
Stanley Pascua <stanleypascua@Stanleys-MBP.lan1>
2020-07-21 16:51:35 -0700

    logout functionality

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -9,11 +9,15 @@ const Navbar = ({ title, icon }) => {
 
     const { isAuthenticated, logout, user } = authContext;
 
+    const onLogout = () => {
+        logout()
+    }
+
     const authLinks = (
         <Fragment>
             <li>Hello { user && user.name }</li>
             <li>
-                <a href="#!">
+                <a onClick={onLogout} href="#!">
                     <i className="fas fa-sign-out-alt"></i> <span className="hide-sm">Logout</span>
                 </a>
             </li>
